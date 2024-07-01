3d28dca28f38afcac457c1924e7d1cd875267b4e
Geethanjalipandu <geethanjaligeddam@gmail.com>
2024-07-01 10:36:24 +0530

    new code2

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,27 @@
-public class Controller {
+
+    public class GameController {
+    import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+    @RestController
+    @RequestMapping("/game")
+    public class GameController {
+
+        @Autowired
+        private GameService gameService;
+
+        @GetMapping("/guess")
+        public String guessNumber(@RequestParam int number) {
+            return gameService.guessNumber(number);
+        }
+
+        @PostMapping("/reset")
+        public String resetGame() {
+            gameService.resetGame();
+            return "Game has been reset. Try to guess the new number.";
+        }
+    }
+
+
+}
 }
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,2 +1,33 @@
 public class Service {
+    public class GameService {
+    import org.springframework.stereotype.Service;
+    import java.util.Random;
+
+    @Service
+    public class GameService {
+        private int numberToGuess;
+        private Random random;
+
+        public GameService() {
+            this.random = new Random();
+            this.numberToGuess = random.nextInt(100) + 1; // Number between 1 and 100
+        }
+
+        public String guessNumber(int guessedNumber) {
+            if (guessedNumber == numberToGuess) {
+                return "Congratulations! You guessed the correct number.";
+            } else if (guessedNumber < numberToGuess) {
+                return "Too low. Try again.";
+            } else {
+                return "Too high. Try again.";
+            }
+
+        }
+    }
+
+    public void resetGame() {
+        this.numberToGuess = random.nextInt(100) + 1;
+    }
+}
+
 }
